1cd38cdf5af8df3b75aff3d523a4602262f35c4d
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-27 14:07:20 +0300

    Finished the header file

--- main.cpp
+++ main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include "encryption.h"
+#include <string>
+#include <fstream> 
 
+using namespace std;
+
+int main() {
+    
+    string filename;
+    char mode;
+
+    cout << "Enter the file name";
+    getline(cin >> ws, filename); // get the file name from the user
+
+    cout << "Encrypt or Decrypt? (E/D)";
+    cin >> mode; // get the mode from the user
+
+    if (mode == 'e' || mode == 'E') {
+        
+    }
+
+    return 0;
+}
 
