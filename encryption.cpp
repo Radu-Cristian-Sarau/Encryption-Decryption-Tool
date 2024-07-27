07bd96d848f12e3abfdaa89c6908ba2212b46675
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-27 14:14:55 +0300

    Defined main function

---
+++ encryption.cpp
@@ -0,0 +1,7 @@
+#include "encryption.h"
+#include <fstream>
+#include <cctype>
+
+bool encryptFile(const string& filename, bool encrypt) {
+    
+}
--- main.cpp
+++ main.cpp
@@ -10,14 +10,26 @@ int main() {
     string filename;
     char mode;
 
-    cout << "Enter the file name";
+    cout << "Enter the file name: " << endl;
     getline(cin >> ws, filename); // get the file name from the user
 
-    cout << "Encrypt or Decrypt? (E/D)";
+    cout << "Encrypt or Decrypt? (E/D)" << endl;
     cin >> mode; // get the mode from the user
 
     if (mode == 'e' || mode == 'E') {
-        
+        if (encryptFile(filename, true)) {
+            cout << "File encrypted successfully." << endl;
+        } else {
+            cerr << "Error: Unable to perform encryption." << endl;
+        }
+    } else if (mode == 'd' || mode == 'D') {
+        if (encryptFile(filename, false)) {
+            cout << "File decrypted successfully." << endl;
+        } else {
+            cerr << "Error: Unable to perform decryption." << endl;
+        }
+    } else {
+        cerr << "Error: Invalid mode selection. Use 'e' for encryption or 'd' for decryption." << endl;
     }
 
     return 0;
